ec1aaf30d817e4b922c1322184052cc71e239aa0
Timo Harmonen <timo.harmonen@iki.fi>
2015-11-22 19:30:43 +0200

    Added counter (max 20), also real server address

--- coffee-agent.sh
+++ coffee-agent.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+
 CACHE_FILE="/tmp/tmp-coffee-agent-values.txt"
+
+if [ -z ${COFFEE_DEBUG} ]; then
+    SERVER="http://172.16.1.22:5000"
+else
+    SERVER="fresh-coffee-server.herokuapp.com"
+fi
+
+SERVICE_URL="${SERVER}/v1/post_data"
 
 
 # one sample consists of N subsamples
@@ -11,8 +20,7 @@ set -x
 
 read_sensor() {
 
-    if [ -z ${COFFEE_DEBUG} ]
-    then
+    if [ -z ${COFFEE_DEBUG} ]; then
         POWER=$(cat /proc/power/active_pwr1)
         POWER=$(awk -v pwr=${POWER} 'BEGIN{print int(pwr)}')
     else
@@ -46,6 +54,7 @@ get_time() {
 }
 
 collect_data() {
+    COUNTER=0
     PREV_VALUE=-1
     VALUES=""
     START_TIME=$(get_time)
@@ -57,14 +66,16 @@ collect_data() {
         else
             VALUES=${VALUES}","${VALUE}
         fi
-        if [ ${VALUE} -ne ${PREV_VALUE} ]; then
+        if [ ${VALUE} -ne ${PREV_VALUE} ] || [ ${COUNTER} -gt 20 ]; then
             RET=$(send_data "${VALUES}" "${START_TIME}" "$(get_time)")
             if [ ${RET} -eq 0 ]; then
+                COUNTER=0
                 VALUES=""
                 START_TIME=$(get_time)
             fi
         fi
-        PREV_VALUE=${VALUE}
+        PREV_VALUE=${VALUE} 
+        COUNTER=$((COUNTER+1))
     done
 }
 
@@ -73,7 +84,7 @@ send_data() {
     START_TIME=$2
     END_TIME=$3
 
-    wget -O - --quiet --post-data "values=${VALUES}&start=${START_TIME}&end=${END_TIME}" http://172.16.1.22:5000/v1/post_data >> ${CACHE_FILE}
+    wget -O - --quiet --post-data "values=${VALUES}&start=${START_TIME}&end=${END_TIME}" ${SERVICE_URL} >> ${CACHE_FILE}
     
     if [ $? -ne 0 ]; then
         echo 1
